203ea6605ca18563824d5ab3d395859a8760ea08
Test <zakharseva1980@gmail.com>
2018-11-18 13:18:11 +0300

    Add project files.

---
+++ SportStor/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportStor.Models;
+
+namespace SportStor.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductContext db = new ProductContext();
+        // GET: Product
+        public ActionResult List()
+        {
+            return View(db.Products.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product pr)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(pr);
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return View(pr);
+        }
+    }
+}
